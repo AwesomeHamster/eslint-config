d268e88e6abb91741a0cb8095c685a3498da8e3d
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-07-05 03:08:50 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # eslint-config
+
 Common ESLint configuration used in Hamster
+
+
+## Install
+
+```bash
+npm i --save-dev eslint @hamster-bot/eslint-config
+# Or
+yarn add --dev eslint @hamster-bot/eslint-config
+```
+
+If you are using TypeScript as well, you need to install typescript-eslint plugin.
+
+```bash
+npm i --save-dev @typescript-eslint/eslint-plugin @typescript-eslint/parser
+# Or
+yarn add --dev @typescript-eslint/eslint-plugin @typescript-eslint/parser
+```
+
+## Usage
+
+Create an `.eslintrc.json` file and append the following:
+
+```json
+{
+  "extends": [
+    "@hamster-bot" // or "@hamster-bot/eslint-config/typescript" for TypeScript users
+  ]
+}
+```
+
+## License
+
+![Creative Commons License](https://i.creativecommons.org/l/by-nc/4.0/88x31.png)
+
+This work is licensed under a [Creative Commons Attribution-NonCommercial 4.0 International License](http://creativecommons.org/licenses/by-nc/4.0/).
---
+++ typescript.eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  extends: ['./index.eslintrc.js'],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': [
+        '.ts',
+      ],
+    },
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    '@typescript-eslint/array-type': 'error',
+    'default-param-last': 'off',
+    '@typescript-eslint/default-param-last': 'error',
+    'func-call-spacing': 'off',
+    '@typescript-eslint/func-call-spacing': 'error',
+    'keyword-spacing': 'off',
+    '@typescript-eslint/keyword-spacing': 'error',
+    'no-redeclare': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'none',
+      },
+    ],
+    'no-dupe-class-members': 'off',
+    '@typescript-eslint/no-dupe-class-members': 'error',
+    'no-inner-declarations': 'off',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'warn',
+    'semi': 'off',
+    '@typescript-eslint/semi': [
+      'error',
+      'never',
+    ],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+        },
+        singleline: {
+          delimiter: 'semi',
+        },
+      },
+    ],
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        asyncArrow: 'always',
+        named: 'never',
+      },
+    ],
+    '@typescript-eslint/type-annotation-spacing': 'error',
+  },
+}
