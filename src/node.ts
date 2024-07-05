51a03f5efb7124f4d3d1602c15adee55f57ff033
Maiko Sinkyaet Tan <maiko.tan.coding@gmail.com>
2024-07-05 13:38:37 +0800

    feat: migrate to flat config (#24)
    * feat: migrate to flat config
    
    * docs: update readme
    
    * chore: update most dependencies

--- src/node.ts
+++ src/node.ts
@@ -1,26 +1,33 @@
+import { FlatCompat } from '@eslint/eslintrc'
 import { Linter } from 'eslint'
+import globals from 'globals'
 
-export default {
-  extends: ['@hamster-bot/eslint-config', 'plugin:n/recommended'],
-  env: {
-    node: true,
+import baseConfig from './index'
+
+const compat = new FlatCompat()
+const config: Linter.FlatConfig[] = [
+  ...baseConfig,
+  ...compat.extends('plugin:n/recommended'),
+  {
+    languageOptions: {
+      ecmaVersion: 2024,
+      globals: globals.node,
+    },
+    rules: {
+      'n/global-require': 'error',
+      'n/no-new-require': 'error',
+      'n/no-unpublished-import': 'off',
+      'n/no-unsupported-features/node-builtins': 'off',
+      'n/no-process-exit': 'off',
+      'n/prefer-global/buffer': 'error',
+      'n/prefer-global/console': 'error',
+      'n/prefer-global/process': 'error',
+      'n/prefer-global/text-decoder': 'error',
+      'n/prefer-global/text-encoder': 'error',
+      'n/prefer-global/url-search-params': 'error',
+      'n/prefer-global/url': 'error',
+    },
   },
-  parserOptions: {
-    ecmaVersion: 2024,
-  },
-  plugins: ['n'],
-  rules: {
-    'n/global-require': 'error',
-    'n/no-new-require': 'error',
-    'n/no-unpublished-import': 'off',
-    'n/no-unsupported-features/node-builtins': 'off',
-    'n/no-process-exit': 'off',
-    'n/prefer-global/buffer': 'error',
-    'n/prefer-global/console': 'error',
-    'n/prefer-global/process': 'error',
-    'n/prefer-global/text-decoder': 'error',
-    'n/prefer-global/text-encoder': 'error',
-    'n/prefer-global/url-search-params': 'error',
-    'n/prefer-global/url': 'error',
-  },
-} satisfies Linter.Config as Linter.Config
+]
+
+export default config
--- src/typescript.ts
+++ src/typescript.ts
@@ -1,60 +1,77 @@
+import { FlatCompat } from '@eslint/eslintrc'
 import { Linter } from 'eslint'
+import tseslint from 'typescript-eslint'
 
-export default {
-  extends: ['@hamster-bot/eslint-config', 'plugin:@typescript-eslint/recommended', 'plugin:import/typescript'],
-  parser: '@typescript-eslint/parser',
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-    'import/resolver': {
-      typescript: true,
+import baseConfig from './index'
+
+const compat = new FlatCompat()
+const config: Linter.FlatConfig[] = [
+  ...baseConfig,
+  ...(tseslint.configs.recommendedTypeChecked as Linter.FlatConfig[]),
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/array-type': 'error',
-    'default-param-last': 'off',
-    '@typescript-eslint/default-param-last': 'error',
-    'func-call-spacing': 'off',
-    '@typescript-eslint/func-call-spacing': 'error',
-    'keyword-spacing': 'off',
-    '@typescript-eslint/keyword-spacing': 'error',
-    'no-redeclare': 'off',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        args: 'none',
+  ...compat.extends('plugin:import/typescript'),
+  {
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
       },
-    ],
-    'no-dupe-class-members': 'off',
-    '@typescript-eslint/no-dupe-class-members': 'error',
-    'no-inner-declarations': 'off',
-    'no-useless-constructor': 'off',
-    '@typescript-eslint/no-useless-constructor': 'warn',
-    'semi': 'off',
-    '@typescript-eslint/semi': ['error', 'never'],
-    '@typescript-eslint/member-delimiter-style': [
-      'error',
-      {
-        multiline: {
-          delimiter: 'none',
+      'import/resolver': {
+        typescript: true,
+      },
+    },
+    rules: {
+      '@typescript-eslint/array-type': 'error',
+      'default-param-last': 'off',
+      '@typescript-eslint/default-param-last': 'error',
+      'func-call-spacing': 'off',
+      '@typescript-eslint/func-call-spacing': 'error',
+      'keyword-spacing': 'off',
+      '@typescript-eslint/keyword-spacing': 'error',
+      'no-redeclare': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'none',
         },
-        singleline: {
-          delimiter: 'semi',
+      ],
+      'no-dupe-class-members': 'off',
+      '@typescript-eslint/no-dupe-class-members': 'error',
+      'no-inner-declarations': 'off',
+      'no-useless-constructor': 'off',
+      '@typescript-eslint/no-useless-constructor': 'warn',
+      'semi': 'off',
+      '@typescript-eslint/semi': ['error', 'never'],
+      '@typescript-eslint/member-delimiter-style': [
+        'error',
+        {
+          multiline: {
+            delimiter: 'none',
+          },
+          singleline: {
+            delimiter: 'semi',
+          },
         },
-      },
-    ],
-    'space-before-function-paren': 'off',
-    '@typescript-eslint/space-before-function-paren': [
-      'error',
-      {
-        anonymous: 'always',
-        asyncArrow: 'always',
-        named: 'never',
-      },
-    ],
-    '@typescript-eslint/type-annotation-spacing': 'error',
+      ],
+      'space-before-function-paren': 'off',
+      '@typescript-eslint/space-before-function-paren': [
+        'error',
+        {
+          anonymous: 'always',
+          asyncArrow: 'always',
+          named: 'never',
+        },
+      ],
+      '@typescript-eslint/type-annotation-spacing': 'error',
+    },
   },
-} satisfies Linter.Config as Linter.Config
+]
+
+export default config
