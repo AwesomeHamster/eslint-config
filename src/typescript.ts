e7cf16da5af17465e0f601f36ccbed2723dd997e
Maiko Tan <maiko.tan.coding@gmail.com>
2024-08-06 15:00:52 +0800

    fix: tseslint removed stylistic rules in 8.0

--- src/typescript.ts
+++ src/typescript.ts
@@ -28,10 +28,6 @@ const config = [
       '@typescript-eslint/array-type': 'error',
       'default-param-last': 'off',
       '@typescript-eslint/default-param-last': 'error',
-      'func-call-spacing': 'off',
-      '@typescript-eslint/func-call-spacing': 'error',
-      'keyword-spacing': 'off',
-      '@typescript-eslint/keyword-spacing': 'error',
       'no-redeclare': 'off',
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': [
@@ -45,29 +41,6 @@ const config = [
       'no-inner-declarations': 'off',
       'no-useless-constructor': 'off',
       '@typescript-eslint/no-useless-constructor': 'warn',
-      'semi': 'off',
-      '@typescript-eslint/semi': ['error', 'never'],
-      '@typescript-eslint/member-delimiter-style': [
-        'error',
-        {
-          multiline: {
-            delimiter: 'none',
-          },
-          singleline: {
-            delimiter: 'semi',
-          },
-        },
-      ],
-      'space-before-function-paren': 'off',
-      '@typescript-eslint/space-before-function-paren': [
-        'error',
-        {
-          anonymous: 'always',
-          asyncArrow: 'always',
-          named: 'never',
-        },
-      ],
-      '@typescript-eslint/type-annotation-spacing': 'error',
     },
   },
 ] as Linter.FlatConfig[]
